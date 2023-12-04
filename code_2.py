0c920b922eb0cbd10e1bce431f6d624480ef64cf
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-12-04 03:26:59 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,66 @@
+    for item in data:
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    main()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def generate_random_data():
+
+    return data
+def main():
+
+if __name__ == "__main__":
+    data = generate_random_data()
+def main():
+def generate_random_data():
+    for item in data:
+
+if __name__ == "__main__":
+    main()
+import random
+        print(f"Random Number: {item}")
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    for item in data:
+import random
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+
+    main()
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+import random
+    for item in data:
+
+    return data
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+if __name__ == "__main__":
+def generate_random_data():
+def generate_random_data():
+if __name__ == "__main__":
+    data = generate_random_data()
+
+
+
+def main():
+    return data
+
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()
