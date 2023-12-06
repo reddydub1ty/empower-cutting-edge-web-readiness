a3f224ba3d801ccf114773f32d7072a7dde67bf3
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-12-06 08:34:02 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+function generateRandomData() {
+    data.forEach(item => {
+
+
+function main() {
+    return data;
+
+    const data = [];
+}
+        console.log("Random Number: " + item);
+main();
+    const data = generateRandomData();
+    }
+    for (let i = 0; i < 10; i++) {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+
+}
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+    }
+        }
